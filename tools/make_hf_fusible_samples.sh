13c2197ba2cd7ef9acec675ecd3fabe76553ff4f
Liu Yiqun <liuyiqun01@baidu.com>
2026-03-04 18:37:11 +0800

    Add 2828 samples.

--- tools/make_hf_fusible_samples.sh
+++ tools/make_hf_fusible_samples.sh
@@ -2,7 +2,6 @@
 
 AI4C_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 
-rm -rf ${AI4C_ROOT}/samples/hf_subgraphs/fusible_subgraphs
 python ${AI4C_ROOT}/tools/generate_ai4c_sample.py \
     --grouped-sample-uids-list "${AI4C_ROOT}/graph_lists/hf_grouped_fusible_subgraph_uids.txt" \
     --model-path-list "${AI4C_ROOT}/graph_lists/hf_fusible_subgraphs.txt" \
