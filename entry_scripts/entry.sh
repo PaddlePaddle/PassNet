3ddc96dcbbcb1b11c4391043dc7061375626a05c
JewelRoam <2752594773@qq.com>
2026-04-29 16:29:19 +0800

    change b from 0.001 to 0.1

--- entry_scripts/entry.sh
+++ entry_scripts/entry.sh
@@ -46,5 +46,5 @@ python3 -m graph_net_bench.aggregate_es_scores \
     --benchmark-path "$OUTPUT_PATH/validation.log" \
     --sample-id 1 \
     --disable-aggregation-mode \
-    --fpdb 0.001 \
+    --fpdb 0.1 \
     --output-json-file-path "$OUTPUT_PATH/aggregated_score.json"
